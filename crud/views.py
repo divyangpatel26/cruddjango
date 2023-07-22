dcb5828d7a7f41174349dd73790aecfca1d0871d
Divyangkumar Patel <divz@Divyangkumars-MacBook-Air.local>
2023-07-22 12:50:45 +0100

    UI designed

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    students = Student.objects.all()
+    context = {"students":students}
+    return render(request,"index.html",context)
